4b879fec41267a88eb06a6e8433a96c2417a0fa8
Zaheen Shahriyar <zaheenshahriyar@Zaheens-MacBook-Air.local>
2020-12-14 23:11:42 -0500

    typing intro

--- src/components/aboutus.js
+++ src/components/aboutus.js
@@ -4,7 +4,9 @@ import React, { Component } from 'react';
 class About extends Component {
 render() {
 return(
-<div><h1>About Page</h1></div>
+<div><h1>About Us</h1>
+<h3>We are a small catering service that provides for any events and love to share our wonderful food with everyone</h3></div>
+
 )
 }
 }
