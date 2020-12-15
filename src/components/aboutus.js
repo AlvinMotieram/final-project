85e01ff6e2da79c67f95abc082765b747b8b3f71
Zaheen Shahriyar <zaheenshahriyar@Zaheens-MacBook-Air.local>
2020-12-14 23:20:41 -0500

    changing intro

--- src/components/aboutus.js
+++ src/components/aboutus.js
@@ -5,7 +5,7 @@ class About extends Component {
 render() {
 return(
 <div><h1>About Us</h1>
-<h3>We are a small catering service that provides for any events and love to share our wonderful food with everyone</h3></div>
+<h3>We are a small catering service that provides for any events and love to share our wonderful food with everyone. Our food is a Bangladeshi/Indian cuisine.  We started this service in March when the pandemic started since we know people in our community needs food or does not have any food supplies to cook their meals.  The pandemic was a reason for us to create an online website, so people can reach out and contact us on what they need.  We try our best to meet their needs and enjoy our cuisines. </h3></div>
 
 )
 }
